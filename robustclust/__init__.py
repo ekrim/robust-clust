d4cf56c638663a1e05d623b973c2753fa6798710
Evan Kriminger <evankriminger@gmail.com>
2018-06-24 03:12:02 -0700

    turning active class discovery into function

--- robustclust/__init__.py
+++ robustclust/__init__.py
@@ -8,7 +8,7 @@ from .utils import plot_constraints, \
 from .active_learning import get_constraints, \
                              FFQS, \
                              MMFFQS, \
-                             ActiveClassDiscovery
+                             active_class_discovery 
 
 from .constrained import E2CP, \
                          SpectralLearning
--- robustclust/active_learning.py
+++ robustclust/active_learning.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sklearn.datasets as ds
+from scipy.spatial.distance import pdist
 from sklearn.cluster import AgglomerativeClustering
 
 from .utils import pdist_idx, \
@@ -11,24 +12,31 @@ from robustclust import plot_labels, \
                         plot_constraints
 
 
-def get_constraints(labels, pdist_vec, method='rand', num_constraints=None, err_rate=0):
+def get_constraints(data, labels, method='rand', num_constraints=None, err_rate=0):
   N = labels.size
+
+  pdist_vec = pdist(data)
 
   # half the number of samples is a good baseline
   num_constraints = N/2 if num_constraints is None else num_constraints
 
-  if method == 'mmffqs':
-    query_mat, clus_label = MMFFQS(labels, pdist_vec, num_constraints)
+  if method == 'acd':
+    query_mat = active_class_discovery(data)
+    big_constraint_mat = None
+    print(query_mat)
+
+  elif method == 'mmffqs':
+    query_mat, clus_label = MMFFQS(pdist_vec, labels, num_constraints)
     big_constraint_mat = all_pairwise(clus_label)
 
-  if method == 'ffqs':
-    query_mat, clus_label = FFQS(labels, pdist_vec, num_constraints)
+  elif method == 'ffqs':
+    query_mat, clus_label = FFQS(pdist_vec, labels, num_constraints)
     big_constraint_mat = all_pairwise(clus_label)
 
   elif method == 'rand':
     query_mat = np.random.randint(0, N, (num_constraints,2))
     big_constraint_mat = None
- 
+  
   else:
     assert False, 'no such method'
 
@@ -44,7 +52,7 @@ def get_constraints(labels, pdist_vec, method='rand', num_constraints=None, err_
   return constraint_mat.astype(int), big_constraint_mat
 
 
-def FFQS(labels, pdist_vec, num_constraints):
+def FFQS(pdist_vec, labels, num_constraints):
   """Furthest first query search
   """
   num_class = np.unique(labels).size
@@ -52,7 +60,7 @@ def FFQS(labels, pdist_vec, num_constraints):
 
   nbr_label = np.zeros(N)
   ind = np.arange(N)
-  nbr_label[np.random.random_integers(0, N-1, 1)] = 1
+  nbr_label[np.random.randint(N)] = 1
   quer_cnt = 0
   constraint_mat = np.zeros((num_constraints,3))
   found_all = False
@@ -88,7 +96,7 @@ def FFQS(labels, pdist_vec, num_constraints):
   return constraint_mat, nbr_label
 
 
-def MMFFQS(labels, pdist_vec, num_constraints):
+def MMFFQS(pdist_vec, labels, num_constraints):
   """minimax furthest first query search
   """
   num_class = np.unique(labels).size
@@ -96,10 +104,10 @@ def MMFFQS(labels, pdist_vec, num_constraints):
   
   paff_vec = affinity(pdist_vec)
 
-  constraint_mat, clus_label = FFQS(labels, pdist_vec, num_constraints)
+  constraint_mat, clus_label = FFQS(pdist_vec, labels, num_constraints)
   constraint_mat.astype('int')  
   all_ind = np.arange(N)
-  explore_constraints = constraint_mat[constraint_mat[:,0]!=0, 0:2].astype('int')
+  explore_constraints = constraint_mat[constraint_mat[:,0] != 0, 0:2].astype('int')
   skeleton_ind = np.unique(explore_constraints.reshape(-1))
   query_cnt = explore_constraints.shape[0]
 
@@ -113,28 +121,20 @@ def MMFFQS(labels, pdist_vec, num_constraints):
       q = candidate_ind[q_ind]
 
     else:
-      q = np.random.random_integers(0,N-1,1)
+      q = np.random.randint(N)
 
     num_clus = clus.size
     sim_vec = np.zeros(num_clus)
     ind_vec = np.zeros(num_clus).astype(int)
     for k in range(num_clus):
       ind_k = all_ind[clus_label == clus[k]]
-      sim_ind = np.argmax(pdist_block(paff_vec, q, ind_k), axis=0)
-      print('--------')
-      print(q)
-      print(ind_k)
-      print(sim_ind)
-      print('--------')
-      sim_vec[k] = pdist_block(paff_vec, q, ind_k)[sim_ind]
+      sim_ind = np.argmax(pdist_block(paff_vec, q, ind_k))
+      sim_vec[k] = pdist_block(paff_vec, q, ind_k).flatten()[sim_ind]
       ind_vec[k] = ind_k[sim_ind]
 
     sort_ind = np.argsort(-sim_vec)
     ind_vec = ind_vec[sort_ind]
     for k in range(num_clus):
-      print(q)
-      print(k)
-      print(ind_vec)
       link = labels[q] == labels[ind_vec[k]]
       constraint_mat[query_cnt,:] = [q, ind_vec[k], link]
       query_cnt += 1
@@ -150,53 +150,44 @@ def MMFFQS(labels, pdist_vec, num_constraints):
   return constraint_mat[:,:2], clus_label
 
 
-class ActiveClassDiscovery:
-  def __init__(self, data, knownInd=None):
-    self.data = data
-    self.N = data.shape[0]
-    self.hierarchical = AgglomerativeClustering(linkage='average')
-    self.hierarchical.fit(data)
-    self.linkage_to_merges()
-    
-    self.knownLabels = -np.ones(self.N)
-    if knownInd is not None:
-      self.knownLabels[knownInd] = 1      
+def active_class_discovery(data, known_ind=None):
+  N = data.shape[0]
+  agg = AgglomerativeClustering(linkage='average')
+  agg.fit(data)
 
-  def get_query(self):    
-    numberOfEmpties = np.zeros((self.N-1,2))
-    for i in range(self.N-1):
-      ind1 = self.mergeHistory[i][0]
-      ind2 = self.mergeHistory[i][1]
-      
-      lab1 = np.unique(self.knownLabels[ind1])
-      lab2 = np.unique(self.knownLabels[ind2])
-      if not np.any(lab1 != -1):
-        numberOfEmpties[i,0] = ind1.size
-      if not np.any(lab2 != -1):
-        numberOfEmpties[i,1] = ind2.size  
-    bestSides = np.argmax(numberOfEmpties, axis=1)
-    biggestEmpty = np.argmax(numberOfEmpties[np.arange(self.N-1),bestSides])    
-    emptySide = bestSides[biggestEmpty]
-    
-    queryGroup = self.mergeHistory[biggestEmpty][emptySide] 
-    bestSample = np.random.random_integers(0,queryGroup.size-1)
-    newQuery = queryGroup[bestSample]
-    self.knownLabels[newQuery] = 1
-    return newQuery  
-
-  def linkage_to_merges(self):
-    self.mergeHistory = []
-    clusMem = [np.asarray([x]) for x in range(self.N)]
-    for i in range(0, self.N-1):
-      group1 = self.hierarchical.children_[i,0]
-      group2 = self.hierarchical.children_[i,1]
-      clusMem += [np.append(clusMem[group1], clusMem[group2])]
-      self.mergeHistory += [[clusMem[group1], clusMem[group2]]]
-    
-
-if __name__ == '__main__':
-  N, num_class, Nquery = (300, 6, 20)
-  data, labels = ds.make_blobs(n_samples=N, n_features=2, centers=num_class)
+  merge_history = []
+  clus_mem = [np.asarray([x]) for x in range(N)]
+  for i in range(0, N-1):
+    group1 = agg.children_[i,0]
+    group2 = agg.children_[i,1]
+    clus_mem += [np.append(clus_mem[group1], clus_mem[group2])]
+    merge_history += [[clus_mem[group1], clus_mem[group2]]]
   
-  constraint_mat, _ = get_constraints(labels, pdist(data), method='rand', num_constraints=20, err_rate=0)
-  plot_constraints(data, constraint_mat)
+  known_labels = -np.ones(N)
+  if known_ind is not None:
+    known_labels[known_ind] = 1      
+
+  num_of_empties = np.zeros((N-1, 2))
+  for i in range(N-1):
+    ind1 = merge_history[i][0]
+    ind2 = merge_history[i][1]
+    
+    lab1 = np.unique(known_labels[ind1])
+    lab2 = np.unique(known_labels[ind2])
+
+    if not np.any(lab1 != -1):
+      num_of_empties[i,0] = ind1.size
+
+    if not np.any(lab2 != -1):
+      num_of_empties[i,1] = ind2.size  
+
+  best_sides = np.argmax(num_of_empties, axis=1)
+  biggest_empty = np.argmax(num_of_empties[np.arange(N-1), best_sides])    
+  empty_side = best_sides[biggest_empty]
+  
+  query_group = merge_history[biggest_empty][empty_side] 
+  best_sample = np.random.randint(query_group.size)
+  new_query = query_group[best_sample]
+  known_labels[new_query] = 1
+
+  return new_query  
